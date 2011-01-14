37004d9ca49683ff4713cdd4287e655269a21824
Steve Wilson <stevew@wolfram.com>
2011-01-14 14:31:13 -0700

    Add class variable log_object and method setClassLog to set the variable.

--- Objects.py
+++ Objects.py
@@ -3,12 +3,18 @@
 class Object(object):
 	"""The Object class."""
 
+	log_object = None
+
 	def __init__(self, **kwargs):
 		if kwargs.has_key("log"):
-			self.log_object = kwargs["log"]
-		else:
-			self.log_object = None
+			Object.log_object = kwargs["log"]
 
 	# End __init__
+
+	def setClassLog(self, log):
+		Object.log_object = log
+
+	# End set_class_log
+	
 
 # End Object
