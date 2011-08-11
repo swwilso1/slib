1d0ebdcb5ef93f484e0f994407fd116a2a474408
Steve Wilson <stevew@wolfram.com>
2011-08-11 12:51:01 -0600

    Fix call to getClassVerbose() in logIfVerbose().

--- Objects.py
+++ Objects.py
@@ -46,7 +46,7 @@ class Object(object):
 	# End enableGlobalDryRun	
 
 	def logIfVerbose(self, arg):
-		if Object.log_object and Object.getClassVerbose() and arg != None:
+		if Object.log_object and Object.getClassVerbose(self) and arg != None:
 			Object.log_object.log(str(arg))
 	# End logIfVerbose
 	
